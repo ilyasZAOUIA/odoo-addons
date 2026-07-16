1a9f2f2004d168698275fdf6e255c5ea437c8a60
ilyas <i.zaouia1097@uca.ac.com>
2026-07-16 14:58:19 +0100

    test: retry après passage HTTPS

--- hello_module/models/hello.py
+++ hello_module/models/hello.py
@@ -18,7 +18,7 @@ class Hello(models.Model):
     _name = 'hello.message'
     _description = 'Hello Message'
 
-    MESSAGE = "Hello People"
+    MESSAGE = "Bonjour tout le monde"
 
     name = fields.Char(string='Name', required=True)
     greeting = fields.Char(
